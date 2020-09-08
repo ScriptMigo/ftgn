9bb35b7ba34be4d4bacee09ddac503fb33bd4ede
Scott Dowling <unheard@unheard.org>
2020-09-08 08:54:38 -0400

    Site working now - hidden db config from git

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,10 +1,42 @@
+import sys
+sys.path.append('/home/dldwvxqr/all_domains/fuckthisguyjeff.com/config') 
+
 from flask import Flask, render_template
+import pymysql.cursors
+import random
+import config
+
 application = Flask(__name__)
- 
+application.debug = True
+
+
 @application.route("/")
-def home():
- return render_template("index.htm")
- #return "Test"
+def main():
+    # Set Database connection string
+    
+    connection = pymysql.connect(host='localhost',
+                                user=config.user,
+                                password=config.password,
+                                db=config.db,
+                                charset='utf8mb4',
+                                cursorclass=pymysql.cursors.DictCursor)
+    
+    # Bring back random record from quote table
+    sql = 'select * from ftgj_quotes order by rand() limit 1'
+
+
+    with connection.cursor() as cursor:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        # Pass returned quote to html template file
+        return render_template('index.htm', data=result[0]['quote'])
+
+    connection.close()
+
+
 
 if __name__ == "__main__":
- application.run(debug=True)
+    application.run()
+
+
+ 
